1f727053b3b05b8340a8c79bbed61d6940dc1e84
Anmol Ananthraj <anmolananthraj20@gmail.com>
2026-04-08 09:14:33 +0530

    Implement search functionality with event listeners for click and Enter key

--- weather.js
+++ weather.js
@@ -1,2 +1,18 @@
 const searchInput = document.querySelector('.search-input');
 const searchIcon = document.querySelector('.search-icon');
+
+searchIcon.addEventListener('click', () => {
+    if (searchInput.value.trim() !== '') {
+        console.log(searchInput.value);
+        searchInput.value = '';
+        searchInput.blur();
+    }
+})
+
+searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && searchInput.value.trim() !== '') {
+        console.log(searchInput.value);
+        searchInput.value = '';
+        searchInput.blur();
+    }
+})
