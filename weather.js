06addc8258c15664864c5d635e2c0903ac0c850a
Anmol Ananthraj <anmolananthraj20@gmail.com>
2026-04-08 12:03:11 +0530

    Update weather information retrieval in search functionality

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ const apiKey = '1c95139e8fe15a7afb49de020f701cfd'
 
 searchIcon.addEventListener('click', () => {
     if (searchInput.value.trim() !== '') {
-        console.log(searchInput.value);
+        console.log(updateWeatherInfo(searchInput.value));
         searchInput.value = '';
         searchInput.blur();
     }
@@ -13,8 +13,24 @@ searchIcon.addEventListener('click', () => {
 
 searchInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter' && searchInput.value.trim() !== '') {
-        console.log(searchInput.value);
+        console.log(updateWeatherInfo(searchInput.value));
         searchInput.value = '';
         searchInput.blur();
     }
 })
+
+async function getFetchData(endPoint, city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`
+
+    const response = await fetch(apiUrl)
+
+    return response.json()
+}
+
+
+async function updateWeatherInfo(city){
+    const weatherData = await getFetchData('weather', city)
+    
+    console.log(weatherData)
+
+}
